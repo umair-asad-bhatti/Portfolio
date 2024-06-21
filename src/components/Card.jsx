b966a617e9df39fa6325fdbc81c2e5f910b0cba3
umair-asad-bhatti <umaircyber6@gmail.com>
2024-06-21 15:34:30 +0500

    fixed card width on mobile devices

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@
 
 export default function Card({ imageLink, title, desctiption, buttonText, technologies, projectLink }) {
     return (
-        <div className="card lg:w-[30vw] w-96  h-auto  border-2 dark:border-neutral shadow  bg-base-100 p-3">
+        <div className="card lg:w-[30vw] md:w-96 w-80  h-auto  border-2 dark:border-neutral shadow  bg-base-100 p-3">
             <div className="w-full h-[200px]"><img className="w-full h-[200px] object-cover rounded-xl" src={imageLink} alt="Shoes" /></div>
             <div className="card-body p-3">
                 <h2 className="card-title">{title}</h2>
